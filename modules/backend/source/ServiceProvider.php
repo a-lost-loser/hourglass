a8349c6c197c8c1561d1f07282a2d68eb0202a14
Christopher Mühl <muehl@msu.biz>
2015-12-23 17:16:07 +0100

    Add Backend and Forum module

---
+++ modules/backend/source/ServiceProvider.php
@@ -0,0 +1,17 @@
+<?php namespace Surgeon\Board\Backend;
+
+use Illuminate\Support\ServiceProvider as ServiceProviderBase;
+
+class ServiceProvider extends ServiceProviderBase
+{
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        // TODO: Implement register() method.
+    }
+}
---
+++ modules/forum/source/ServiceProvider.php
@@ -0,0 +1,17 @@
+<?php namespace Surgeon\Board\Forum;
+
+use Illuminate\Support\ServiceProvider as ServiceProviderBase;
+
+class ServiceProvider extends ServiceProviderBase
+{
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        // TODO: Implement register() method.
+    }
+}
