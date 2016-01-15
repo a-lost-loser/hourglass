eb31a259ed6e72290c472489486d81acc00c435c
Christopher Mühl <muehl@msu.biz>
2016-01-15 14:49:05 +0100

    Add user model to backend

---
+++ modules/backend/src/Models/User.php
@@ -0,0 +1,14 @@
+<?php namespace Exoplanet\Backend\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class User extends Model
+{
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+}
