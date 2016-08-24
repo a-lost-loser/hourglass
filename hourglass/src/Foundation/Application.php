30c9e8c007ccd43ec6e36884a60e3f8fbaa8097d
Christopher Mühl <christopher@padarom.io>
2016-08-24 14:05:10 +0200

    Add user model

--- hourglass/src/Foundation/Application.php
+++ hourglass/src/Foundation/Application.php
@@ -4,5 +4,8 @@ use Illuminate\Foundation\Application as ApplicationBase;
 
 class Application extends ApplicationBase
 {
-
+    public function databasePath()
+    {
+        return $this->databasePath ?: $this->basePath.DIRECTORY_SEPARATOR.'hourglass'.DIRECTORY_SEPARATOR.'database';
+    }
 }
