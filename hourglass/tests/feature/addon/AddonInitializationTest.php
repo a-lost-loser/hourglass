824f7deaa151c4230e09d6e157b5cf4d8b7fd92b
Christopher Mühl <christopher@padarom.io>
2016-08-29 15:58:19 +0200

    Streamline testing

--- hourglass/tests/feature/addon/AddonInitializationTest.php
+++ hourglass/tests/feature/addon/AddonInitializationTest.php
@@ -8,12 +8,11 @@ class AddonInitializationTest extends TestCase
 {
     public function test_it_can_load_arbitrary_addons()
     {
-        $stub = Mockery::mock(Addon::class)
-            ->makePartial()
-            ->shouldReceive('enable')->once()
-            ->getMock();
+        $stub = Mockery::spy(Addon::class);
 
         $repository = $this->app->make(AddonRepository::class);
         $repository->enableAddon($stub);
+
+        $stub->shouldHaveReceived('enable')->once();
     }
 }
